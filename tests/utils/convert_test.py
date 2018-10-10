1df77b14d18f934127686bb4cc226d4ff0f29ee9
YanWQ-monad <YanWQmonad@gmail.com>
2018-10-10 22:09:02 +0800

    Add function to utils.convert
    Add function:
    - dict_array
    - array_dict
    
    dict <==> array convert

--- tests/utils/convert_test.py
+++ tests/utils/convert_test.py
@@ -20,3 +20,39 @@ class TestConvert(TestCase):
     def test_to_int_default(self):
         number = convert.to_int('none', default=1013)
         self.assertEqual(number, 1013)
+
+    def test_extend_dict(self):
+        dict1 = {'a': 1, 'b': 2}
+        dict2 = {'b': 3, 'c': 4}
+
+        new_dict = convert.extend_dict(dict1, dict2)
+        self.assertEqual(new_dict['a'], 1)
+        self.assertEqual(new_dict['b'], 3)
+
+        new_dict = convert.extend_dict(dict2, dict1)
+        self.assertEqual(new_dict['b'], 2)
+
+    def test_array_dict_normal(self):
+        array = [1, 2, 3]
+        new_dict = convert.array_dict(array, ['b', 'a', 'c'])
+
+        expect_dict = {'a': 2, 'b': 1, 'c': 3}
+        self.assertDictEqual(new_dict, expect_dict)
+
+    def test_array_dict_with_empty_key(self):
+        array = [1, 2, 3]
+        new_dict = convert.array_dict(array, ['a', 'b', ''])
+
+        self.assertFalse(3 in new_dict.values())
+
+    def test_dict_array_normal(self):
+        origin_dict = {'a': 2, 'b': 1, 'c': 3}
+        array = convert.dict_array(origin_dict, ['c', 'b', 'a'])
+
+        self.assertListEqual(array, [3, 1, 2])
+
+    def test_dict_array_with_missing_key(self):
+        origin_dict = {'a': 2, 'b': 1, 'c': 3}
+        array = convert.dict_array(origin_dict, ['a', 'b', 'd'])
+
+        self.assertIsNone(array[2])
--- utils/convert.py
+++ utils/convert.py
@@ -18,3 +18,21 @@ def extend_dict(*args):
     for arg in args:
         result.update(arg)
     return result
+
+
+def array_dict(arr, order):
+    assert len(arr) == len(order)
+    ret = dict(zip(order, arr))
+    if '' in ret:
+        del ret['']
+    return ret
+
+
+def dict_array(d, order):
+    arr = [None for _ in range(len(order))]
+
+    for (k, v) in d.items():
+        if k in order:
+            arr[order.index(k)] = v
+
+    return arr
